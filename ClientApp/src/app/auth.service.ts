da023337ba956bcad912fe25082ee4bbfe65889d
mateomen1996 <mateomen1996@hotmail.com>
2020-07-18 18:25:29 -0400

    asd

--- ClientApp/src/app/auth.service.ts
+++ ClientApp/src/app/auth.service.ts
@@ -1,14 +1,11 @@
 import { Injectable } from '@angular/core';
 import { GooglePlus} from "@ionic-native/google-plus/ngx"
+import { auth } from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
   constructor(private google:GooglePlus) { }
-  loginWithGoogle(){
-    this.google.login({}).then(result=>{
-      return result;
-    })
-  }
+
 }
