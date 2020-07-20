07ff5b5a42837a44d69672adb97b2b8898452e87
unknown <jeffcristhlb@gmail.com>
2020-07-19 22:48:55 -0400

    lista

--- CommerceApp/src/app/tab2/tab2.page.ts
+++ CommerceApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Combo } from "../interfaces/combo";
+import { ComboService } from "../services/combo.service";
+import { AuthService } from "../services/auth.service";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { Storage } from "@ionic/storage";
+const TOKEN_KEY = "access_token";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  combos: Combo[];
+  public promocion: boolean = false;
+  idcomercio = null;
 
-  constructor() {}
-
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private comboService: ComboService,
+    private authService: AuthService,
+    private storage: Storage
+  ) {
+    this.get();
+  }
+  get() {
+    this.storage.get(TOKEN_KEY).then((res) => {
+      this.comboService.get(res).subscribe((data: Combo[]) => {
+        this.combos = data;
+        console.log(this.combos);
+      });
+    });
+  }
 }
