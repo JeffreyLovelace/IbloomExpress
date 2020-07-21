54e74aa937bf393d40e0b9e4475b6aa9a4073448
unknown <jeffcristhlb@gmail.com>
2020-07-21 00:36:28 -0400

    b

--- CommerceApp/src/app/tab2/tab2.page.ts
+++ CommerceApp/src/app/tab2/tab2.page.ts
@@ -4,6 +4,8 @@ import { ComboService } from "../services/combo.service";
 import { AuthService } from "../services/auth.service";
 import { Router, ActivatedRoute, Params } from "@angular/router";
 import { Storage } from "@ionic/storage";
+import { ComercioService } from "../services/comercio.service";
+
 const TOKEN_KEY = "access_token";
 
 @Component({
@@ -15,21 +17,51 @@ export class Tab2Page {
   combos: Combo[];
   public promocion: boolean = false;
   idcomercio = null;
+  correo = null;
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private comboService: ComboService,
     private authService: AuthService,
-    private storage: Storage
+    private storage: Storage,
+    private comercioService: ComercioService
   ) {
-    this.get();
+    this.getCorreo();
   }
   get() {
+    console.log(this.idcomercio);
+
     this.storage.get(TOKEN_KEY).then((res) => {
       this.comboService.get(res).subscribe((data: Combo[]) => {
+        // for (let datos of data) {
+        //  if (1 == datos.id_comercio) {
         this.combos = data;
+        //    console.log("mio");
+
+        //   }
+        // }
         console.log(this.combos);
+      });
+    });
+  }
+  getCorreo() {
+    this.storage.get(TOKEN_KEY).then((res) => {
+      this.authService.getUser(res).subscribe((response) => {
+        this.correo = response.email;
+        this.getId();
+      });
+    });
+  }
+  getId() {
+    this.storage.get(TOKEN_KEY).then((res) => {
+      this.comercioService.get(res).subscribe((data) => {
+        for (let datos of data) {
+          if (datos.correo == this.correo) {
+            this.idcomercio = datos.id;
+            this.get();
+          }
+        }
       });
     });
   }
