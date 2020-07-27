d165d4008cc37b8098f1916d8db27775f5ba36dd
unknown <jeffcristhlb@gmail.com>
2020-07-26 23:49:50 -0400

    app driver

--- DriverApp/src/app/services/driver.service.ts
+++ DriverApp/src/app/services/driver.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Platform } from "@ionic/angular";
+import { Injectable } from "@angular/core";
+import { Storage } from "@ionic/storage";
+import { BehaviorSubject, Observable, from, of } from "rxjs";
+import { take, map, switchMap } from "rxjs/operators";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { environment } from "../../environments/environment";
+const helper = new JwtHelperService();
+const TOKEN_KEY = "access_token";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DriverService {
+  token = null;
+  servidor = environment.url;
+  constructor(
+    private storage: Storage,
+    private http: HttpClient,
+    private plt: Platform,
+    private router: Router,
+    private alertCtrl: AlertController
+  ) {}
 
-  constructor() { }
+  get(token): Observable<any> {
+    let headers = new HttpHeaders().set("Authorization", token);
+    return this.http.get(`${this.servidor}/api/conductor`, {
+      headers: headers,
+    });
+  }
 }
--- DriverApp/src/app/tab2/tab2.page.ts
+++ DriverApp/src/app/tab2/tab2.page.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonSlides } from "@ionic/angular";
+import { Combo } from "../interfaces/combo";
+import { ComboService } from "../services/combo.service";
+import { Storage } from "@ionic/storage";
+import { Router, ActivatedRoute } from "@angular/router";
+import { environment } from "../../environments/environment";
+import { ComercioService } from "../services/comercio.service";
+import { PedidoService } from "../services/pedido.service";
+import { Location } from "@angular/common";
+import { AlertController } from "@ionic/angular";
+
+import { OrdenService } from "../services/orden.service";
+import { AuthService } from "../services/auth.service";
+import { ClienteService } from "../services/cliente.service";
+import { Pedido } from "../interfaces/pedido";
+import { Orden } from "../interfaces/orden";
+import { DriverService } from "../services/driver.service";
+
+const TOKEN_KEY = "access_token";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  pedidos: Pedido[];
+  id_client;
+  c = 0;
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private storage: Storage,
+    private comercioService: ComercioService,
+    private comboService: ComboService,
+    private pedidoService: PedidoService,
+    private ordenService: OrdenService,
+    private location: Location,
+    private authService: AuthService,
+    private driverService: DriverService,
+    private alertController: AlertController
+  ) {}
+  ionViewWillEnter() {
+    this.getUser();
+  }
 
-  constructor() {}
+  gePedido() {
+    this.storage.get(TOKEN_KEY).then((res) => {
+      this.pedidoService.get(res).subscribe((data: Pedido[]) => {
+        this.pedidos = data;
+        console.log(this.pedidos);
+      });
+    });
+  }
 
+  getUser() {
+    this.storage.get(TOKEN_KEY).then((res) => {
+      this.authService.getUser(res).subscribe((data) => {
+        console.log(data);
+
+        this.getCliente(data.email);
+      });
+    });
+  }
+  getCliente(correo) {
+    this.storage.get(TOKEN_KEY).then((res) => {
+      this.driverService.get(res).subscribe((data) => {
+        for (let cliente of data) {
+          if (cliente.correo == correo) {
+            this.id_client = cliente.id;
+            console.log(this.id_client);
+            this.gePedido();
+          }
+        }
+        console.log(data);
+      });
+    });
+  }
 }
