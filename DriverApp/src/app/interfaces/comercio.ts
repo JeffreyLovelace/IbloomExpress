c8a455599fae8da16a5076d612f16d169dfe8b45
unknown <jeffcristhlb@gmail.com>
2020-07-26 22:29:22 -0400

    servicios driver y login

---
+++ DriverApp/src/app/interfaces/comercio.ts
@@ -0,0 +1,18 @@
+export class Comercio {
+  constructor(
+    public id: number,
+
+    public correo: number,
+    public telefono: string,
+    public nombre: string,
+    public id_tipoComercio: string,
+    public fotoLogo: string,
+    public fotoBaner: string,
+    public envio: string,
+    public direccion: string,
+    public referencia: string,
+    public precioMinimo: string,
+    public longitud: string,
+    public latitud: string
+  ) {}
+}
